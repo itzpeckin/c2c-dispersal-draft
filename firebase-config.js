68a0d538b582392d72fa06df72a9e3073c704af1
itzpeckin <justinrmandaro@gmail.com>
2026-07-16 00:58:35 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCGTUy3dfuf9um7PewoPDzXuZMrcxCzK8M",
+  authDomain: "c2c-dispersal-draft.firebaseapp.com",
+  projectId: "c2c-dispersal-draft",
+  storageBucket: "c2c-dispersal-draft.firebasestorage.app",
+  messagingSenderId: "374895402695",
+  appId: "1:374895402695:web:f372bb1c5a54da0fada8e2"
+};
